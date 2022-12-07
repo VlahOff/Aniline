d8735e2b2ab930026f1ec250148689c4e54496ae
Dimitar Vlahov <dimitarvlahov24@gmail.com>
2022-12-07 18:41:06 +0200

    Finished the login and register forms

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { LogoutComponent } from './logout/logout.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AuthRoutingModule } from './auth-routing.module';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     AuthRoutingModule
   ]
 })
--- client/src/app/auth/register/register.component.ts
+++ client/src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  registerForm!: FormGroup;
 
   ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'pass': new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      'rePass': new FormControl(null, [Validators.required, Validators.minLength(6), this.doPasswordsMatch.bind(this)])
+    });
   }
 
+  onSubmit() {
+    console.log(this.registerForm);
+  }
+
+  doPasswordsMatch(control: FormControl): { [s: string]: boolean; } | null {
+    if (control.value === this.registerForm?.get('pass')?.value) {
+      return null;
+    }
+    return { 'passwordDontMatch': true };
+  }
 }
