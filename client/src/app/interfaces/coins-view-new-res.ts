613676d5d9c08f36099b19bd0a4f4337e04e2799
Dimitar Vlahov <dimitarvlahov24@gmail.com>
2022-12-15 20:18:18 +0200

    Updated interface

--- client/src/app/interfaces/coins-view-new-res.ts
+++ client/src/app/interfaces/coins-view-new-res.ts
@@ -1,19 +1,14 @@
 export interface CoinsViewNewResponse {
-  id: 22842,
-  name: 'DeFi²',
-  symbol: 'DFX',
-  slug: 'defi-squared',
-  num_market_pairs: 2,
-  date_added: '2022-12-01T16:37:44.000Z',
-  tags: [],
-  max_supply: 989041,
-  circulating_supply: 0,
-  total_supply: 989041,
-  platform: [Object],
-  cmc_rank: 4825,
-  self_reported_circulating_supply: 707576,
-  self_reported_market_cap: 76965.12964945425,
-  tvl_ratio: null,
-  last_updated: '2022-12-15T16:19:00.000Z',
-  quote: [Object];
+  id: string,
+  name: string,
+  symbol: string,
+  total_supply: number,
+  last_updated: Date,
+  price: number,
+  volume_24h: number,
+  volume_change_24h: number,
+  percent_change_1h: number,
+  percent_change_24h: number,
+  percent_change_7d: number;
+
 }
