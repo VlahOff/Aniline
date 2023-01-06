3926f472d7c23dd412bcd76fafd4050b673e3370
Dimitar Vlahov <dimitarvlahov24@gmail.com>
2023-01-06 12:40:00 +0200

    .

--- server/service/tokenService.js
+++ server/service/tokenService.js
@@ -25,15 +25,12 @@ function createToken(user) {
         username: user.username
     };
 
-    const t = new Date();
-    t.setHours(new Date().getHours() + 8);
-
     return {
         userId: user._id,
         email: user.email,
         username: user.username,
         accessToken: jwt.sign(data, JWT_SECRET_TOKEN),
-        expiriesIn: Date.parse(t)
+        expiriesIn: '28800000'
     };
 }
 
