92245c17b2ac73da9bc300e4eb4ac0038a52cce2
Dimitar Vlahov <dimitarvlahov24@gmail.com>
2023-04-19 14:31:45 +0300

    .

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   authApi: 'https://rose-frantic-butterfly.cyclic.app/auth',
-  cryptoApi: 'https://calm-plum-greyhound-garb.cyclic.app/crypto',
-  portfolioApi: 'https://calm-plum-greyhound-garb.cyclic.app/portfolio'
+  cryptoApi: 'https://lucky-cowboy-hat-slug.cyclic.app/crypto',
+  portfolioApi: 'https://lucky-cowboy-hat-slug.cyclic.app/portfolio'
 };
