cf8737e2035d5504eec6973f77a7226427e98e56
Dimitar Vlahov <dimitarvlahov24@gmail.com>
2023-06-02 23:40:30 +0300

    .

--- server/middlewares/corsWhiteList.js
+++ server/middlewares/corsWhiteList.js
@@ -1,8 +1,6 @@
 module.exports = Object.freeze({
   corsWhiteList: [
     'https://aniline.vercel.app',
-    'https://aniline2.vercel.app',
-    'http://localhost:4200',
-    'http://localhost:5173',
+    'https://aniline-old.vercel.app',
   ]
 }); 
